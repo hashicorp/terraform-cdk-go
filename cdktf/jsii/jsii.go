63f8e7548cd79605cf8b9ecaeabf0eb6e5bfc398
CDK for Terraform Team <github-team-tf-cdk@hashicorp.com>
2021-09-30 07:33:35 +0000

    chore(release): v0.6.3-pre.359

--- cdktf/jsii/jsii.go
+++ cdktf/jsii/jsii.go
@@ -12,7 +12,7 @@ import (
 	constructs "github.com/aws/constructs-go/constructs/v10/jsii"
 )
 
-//go:embed cdktf-0.6.3-pre.357.tgz
+//go:embed cdktf-0.6.3-pre.359.tgz
 var tarball []byte
 
 // Initialize loads the necessary packages in the @jsii/kernel to support the enclosing module.
@@ -22,5 +22,5 @@ func Initialize() {
 	constructs.Initialize()
 
 	// Load this library into the kernel
-	_jsii_.Load("cdktf", "0.6.3-pre.357", tarball)
+	_jsii_.Load("cdktf", "0.6.3-pre.359", tarball)
 }
