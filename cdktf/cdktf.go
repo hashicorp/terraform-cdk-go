2d440e91bc4a38130ad676b82526cb0cdb17822a
CDK for Terraform Team <github-team-tf-cdk@hashicorp.com>
2022-05-06 18:49:45 +0000

    chore(release): v0.11.0-pre.40

--- cdktf/cdktf.go
+++ cdktf/cdktf.go
@@ -7465,56 +7465,127 @@ type DataTerraformRemoteStateS3Config struct {
 	Defaults *map[string]interface{} `json:"defaults" yaml:"defaults"`
 	// Experimental.
 	Workspace *string `json:"workspace" yaml:"workspace"`
+	// Name of the S3 Bucket.
 	// Experimental.
 	Bucket *string `json:"bucket" yaml:"bucket"`
+	// Path to the state file inside the S3 Bucket.
+	//
+	// When using a non-default workspace, the state path will be /workspace_key_prefix/workspace_name/key.
 	// Experimental.
 	Key *string `json:"key" yaml:"key"`
+	// (Optional) AWS access key.
+	//
+	// If configured, must also configure secret_key.
+	// This can also be sourced from
+	// the AWS_ACCESS_KEY_ID environment variable,
+	// AWS shared credentials file (e.g. ~/.aws/credentials),
+	// or AWS shared configuration file (e.g. ~/.aws/config).
 	// Experimental.
 	AccessKey *string `json:"accessKey" yaml:"accessKey"`
+	// (Optional) Canned ACL to be applied to the state file.
 	// Experimental.
 	Acl *string `json:"acl" yaml:"acl"`
+	// (Optional) IAM Policy JSON describing further restricting permissions for the IAM Role being assumed.
 	// Experimental.
 	AssumeRolePolicy *string `json:"assumeRolePolicy" yaml:"assumeRolePolicy"`
+	// (Optional) Custom endpoint for the AWS DynamoDB API.
+	//
+	// This can also be sourced from the AWS_DYNAMODB_ENDPOINT environment variable.
 	// Experimental.
 	DynamodbEndpoint *string `json:"dynamodbEndpoint" yaml:"dynamodbEndpoint"`
+	// (Optional) Name of DynamoDB Table to use for state locking and consistency.
+	//
+	// The table must have a partition key named LockID with type of String.
+	// If not configured, state locking will be disabled.
 	// Experimental.
 	DynamodbTable *string `json:"dynamodbTable" yaml:"dynamodbTable"`
+	// (Optional) Enable server side encryption of the state file.
 	// Experimental.
 	Encrypt *bool `json:"encrypt" yaml:"encrypt"`
+	// (Optional) Custom endpoint for the AWS S3 API.
+	//
+	// This can also be sourced from the AWS_S3_ENDPOINT environment variable.
 	// Experimental.
 	Endpoint *string `json:"endpoint" yaml:"endpoint"`
+	// (Optional) External identifier to use when assuming the role.
 	// Experimental.
 	ExternalId *string `json:"externalId" yaml:"externalId"`
+	// (Optional) Enable path-style S3 URLs  (https://<HOST>/<BUCKET> instead of https://<BUCKET>.<HOST>).
 	// Experimental.
 	ForcePathStyle *bool `json:"forcePathStyle" yaml:"forcePathStyle"`
+	// (Optional) Custom endpoint for the AWS Identity and Access Management (IAM) API.
+	//
+	// This can also be sourced from the AWS_IAM_ENDPOINT environment variable.
 	// Experimental.
 	IamEndpoint *string `json:"iamEndpoint" yaml:"iamEndpoint"`
+	// (Optional) Amazon Resource Name (ARN) of a Key Management Service (KMS) Key  to use for encrypting the state.
+	//
+	// Note that if this value is specified,
+	// Terraform will need kms:Encrypt, kms:Decrypt and kms:GenerateDataKey permissions on this KMS key.
 	// Experimental.
 	KmsKeyId *string `json:"kmsKeyId" yaml:"kmsKeyId"`
+	// (Optional) The maximum number of times an AWS API request is retried on retryable failure.
+	//
+	// Defaults to 5.
 	// Experimental.
 	MaxRetries *float64 `json:"maxRetries" yaml:"maxRetries"`
+	// (Optional) Name of AWS profile in AWS shared credentials file (e.g. ~/.aws/credentials)  or AWS shared configuration file (e.g. ~/.aws/config) to use for credentials  and/or configuration. This can also be sourced from the AWS_PROFILE environment variable.
 	// Experimental.
 	Profile *string `json:"profile" yaml:"profile"`
+	// AWS Region of the S3 Bucket and DynamoDB Table (if used).
+	//
+	// This can also
+	// be sourced from the AWS_DEFAULT_REGION and AWS_REGION environment
+	// variables.
 	// Experimental.
 	Region *string `json:"region" yaml:"region"`
+	// (Optional) Amazon Resource Name (ARN) of the IAM Role to assume.
 	// Experimental.
 	RoleArn *string `json:"roleArn" yaml:"roleArn"`
+	// (Optional) AWS secret access key.
+	//
+	// If configured, must also configure access_key.
+	// This can also be sourced from
+	// the AWS_SECRET_ACCESS_KEY environment variable,
+	// AWS shared credentials file (e.g. ~/.aws/credentials),
+	// or AWS shared configuration file (e.g. ~/.aws/config)
 	// Experimental.
 	SecretKey *string `json:"secretKey" yaml:"secretKey"`
+	// (Optional) Session name to use when assuming the role.
 	// Experimental.
 	SessionName *string `json:"sessionName" yaml:"sessionName"`
+	// (Optional) Path to the AWS shared credentials file.
+	//
+	// Defaults to ~/.aws/credentials.
 	// Experimental.
 	SharedCredentialsFile *string `json:"sharedCredentialsFile" yaml:"sharedCredentialsFile"`
+	// (Optional) Skip credentials validation via the STS API.
 	// Experimental.
 	SkipCredentialsValidation *bool `json:"skipCredentialsValidation" yaml:"skipCredentialsValidation"`
+	// (Optional) Skip usage of EC2 Metadata API.
 	// Experimental.
 	SkipMetadataApiCheck *bool `json:"skipMetadataApiCheck" yaml:"skipMetadataApiCheck"`
+	// (Optional) The key to use for encrypting state with Server-Side Encryption with Customer-Provided Keys (SSE-C).
+	//
+	// This is the base64-encoded value of the key, which must decode to 256 bits.
+	// This can also be sourced from the AWS_SSE_CUSTOMER_KEY environment variable,
+	// which is recommended due to the sensitivity of the value.
+	// Setting it inside a terraform file will cause it to be persisted to disk in terraform.tfstate.
 	// Experimental.
 	SseCustomerKey *string `json:"sseCustomerKey" yaml:"sseCustomerKey"`
+	// (Optional) Custom endpoint for the AWS Security Token Service (STS) API.
+	//
+	// This can also be sourced from the AWS_STS_ENDPOINT environment variable.
 	// Experimental.
 	StsEndpoint *string `json:"stsEndpoint" yaml:"stsEndpoint"`
+	// (Optional) Multi-Factor Authentication (MFA) token.
+	//
+	// This can also be sourced from the AWS_SESSION_TOKEN environment variable.
 	// Experimental.
 	Token *string `json:"token" yaml:"token"`
+	// (Optional) Prefix applied to the state path inside the bucket.
+	//
+	// This is only relevant when using a non-default workspace. Defaults to env:
 	// Experimental.
 	WorkspaceKeyPrefix *string `json:"workspaceKeyPrefix" yaml:"workspaceKeyPrefix"`
 }
@@ -14475,58 +14546,144 @@ func (s *jsiiProxy_S3Backend) ToTerraform() interface{} {
 	return returns
 }
 
+// Stores the state as a given key in a given bucket on Amazon S3.
+//
+// This backend
+// also supports state locking and consistency checking via Dynamo DB, which
+// can be enabled by setting the dynamodb_table field to an existing DynamoDB
+// table name. A single DynamoDB table can be used to lock multiple remote
+// state files. Terraform generates key names that include the values of the
+// bucket and key variables.
+//
+// Warning! It is highly recommended that you enable Bucket Versioning on the
+// S3 bucket to allow for state recovery in the case of accidental deletions
+// and human error.
+//
+// Read more about this backend in the Terraform docs:
+// https://www.terraform.io/language/settings/backends/s3
 // Experimental.
 type S3BackendProps struct {
+	// Name of the S3 Bucket.
 	// Experimental.
 	Bucket *string `json:"bucket" yaml:"bucket"`
+	// Path to the state file inside the S3 Bucket.
+	//
+	// When using a non-default workspace, the state path will be /workspace_key_prefix/workspace_name/key.
 	// Experimental.
 	Key *string `json:"key" yaml:"key"`
+	// (Optional) AWS access key.
+	//
+	// If configured, must also configure secret_key.
+	// This can also be sourced from
+	// the AWS_ACCESS_KEY_ID environment variable,
+	// AWS shared credentials file (e.g. ~/.aws/credentials),
+	// or AWS shared configuration file (e.g. ~/.aws/config).
 	// Experimental.
 	AccessKey *string `json:"accessKey" yaml:"accessKey"`
+	// (Optional) Canned ACL to be applied to the state file.
 	// Experimental.
 	Acl *string `json:"acl" yaml:"acl"`
+	// (Optional) IAM Policy JSON describing further restricting permissions for the IAM Role being assumed.
 	// Experimental.
 	AssumeRolePolicy *string `json:"assumeRolePolicy" yaml:"assumeRolePolicy"`
+	// (Optional) Custom endpoint for the AWS DynamoDB API.
+	//
+	// This can also be sourced from the AWS_DYNAMODB_ENDPOINT environment variable.
 	// Experimental.
 	DynamodbEndpoint *string `json:"dynamodbEndpoint" yaml:"dynamodbEndpoint"`
+	// (Optional) Name of DynamoDB Table to use for state locking and consistency.
+	//
+	// The table must have a partition key named LockID with type of String.
+	// If not configured, state locking will be disabled.
 	// Experimental.
 	DynamodbTable *string `json:"dynamodbTable" yaml:"dynamodbTable"`
+	// (Optional) Enable server side encryption of the state file.
 	// Experimental.
 	Encrypt *bool `json:"encrypt" yaml:"encrypt"`
+	// (Optional) Custom endpoint for the AWS S3 API.
+	//
+	// This can also be sourced from the AWS_S3_ENDPOINT environment variable.
 	// Experimental.
 	Endpoint *string `json:"endpoint" yaml:"endpoint"`
+	// (Optional) External identifier to use when assuming the role.
 	// Experimental.
 	ExternalId *string `json:"externalId" yaml:"externalId"`
+	// (Optional) Enable path-style S3 URLs  (https://<HOST>/<BUCKET> instead of https://<BUCKET>.<HOST>).
 	// Experimental.
 	ForcePathStyle *bool `json:"forcePathStyle" yaml:"forcePathStyle"`
+	// (Optional) Custom endpoint for the AWS Identity and Access Management (IAM) API.
+	//
+	// This can also be sourced from the AWS_IAM_ENDPOINT environment variable.
 	// Experimental.
 	IamEndpoint *string `json:"iamEndpoint" yaml:"iamEndpoint"`
+	// (Optional) Amazon Resource Name (ARN) of a Key Management Service (KMS) Key  to use for encrypting the state.
+	//
+	// Note that if this value is specified,
+	// Terraform will need kms:Encrypt, kms:Decrypt and kms:GenerateDataKey permissions on this KMS key.
 	// Experimental.
 	KmsKeyId *string `json:"kmsKeyId" yaml:"kmsKeyId"`
+	// (Optional) The maximum number of times an AWS API request is retried on retryable failure.
+	//
+	// Defaults to 5.
 	// Experimental.
 	MaxRetries *float64 `json:"maxRetries" yaml:"maxRetries"`
+	// (Optional) Name of AWS profile in AWS shared credentials file (e.g. ~/.aws/credentials)  or AWS shared configuration file (e.g. ~/.aws/config) to use for credentials  and/or configuration. This can also be sourced from the AWS_PROFILE environment variable.
 	// Experimental.
 	Profile *string `json:"profile" yaml:"profile"`
+	// AWS Region of the S3 Bucket and DynamoDB Table (if used).
+	//
+	// This can also
+	// be sourced from the AWS_DEFAULT_REGION and AWS_REGION environment
+	// variables.
 	// Experimental.
 	Region *string `json:"region" yaml:"region"`
+	// (Optional) Amazon Resource Name (ARN) of the IAM Role to assume.
 	// Experimental.
 	RoleArn *string `json:"roleArn" yaml:"roleArn"`
+	// (Optional) AWS secret access key.
+	//
+	// If configured, must also configure access_key.
+	// This can also be sourced from
+	// the AWS_SECRET_ACCESS_KEY environment variable,
+	// AWS shared credentials file (e.g. ~/.aws/credentials),
+	// or AWS shared configuration file (e.g. ~/.aws/config)
 	// Experimental.
 	SecretKey *string `json:"secretKey" yaml:"secretKey"`
+	// (Optional) Session name to use when assuming the role.
 	// Experimental.
 	SessionName *string `json:"sessionName" yaml:"sessionName"`
+	// (Optional) Path to the AWS shared credentials file.
+	//
+	// Defaults to ~/.aws/credentials.
 	// Experimental.
 	SharedCredentialsFile *string `json:"sharedCredentialsFile" yaml:"sharedCredentialsFile"`
+	// (Optional) Skip credentials validation via the STS API.
 	// Experimental.
 	SkipCredentialsValidation *bool `json:"skipCredentialsValidation" yaml:"skipCredentialsValidation"`
+	// (Optional) Skip usage of EC2 Metadata API.
 	// Experimental.
 	SkipMetadataApiCheck *bool `json:"skipMetadataApiCheck" yaml:"skipMetadataApiCheck"`
+	// (Optional) The key to use for encrypting state with Server-Side Encryption with Customer-Provided Keys (SSE-C).
+	//
+	// This is the base64-encoded value of the key, which must decode to 256 bits.
+	// This can also be sourced from the AWS_SSE_CUSTOMER_KEY environment variable,
+	// which is recommended due to the sensitivity of the value.
+	// Setting it inside a terraform file will cause it to be persisted to disk in terraform.tfstate.
 	// Experimental.
 	SseCustomerKey *string `json:"sseCustomerKey" yaml:"sseCustomerKey"`
+	// (Optional) Custom endpoint for the AWS Security Token Service (STS) API.
+	//
+	// This can also be sourced from the AWS_STS_ENDPOINT environment variable.
 	// Experimental.
 	StsEndpoint *string `json:"stsEndpoint" yaml:"stsEndpoint"`
+	// (Optional) Multi-Factor Authentication (MFA) token.
+	//
+	// This can also be sourced from the AWS_SESSION_TOKEN environment variable.
 	// Experimental.
 	Token *string `json:"token" yaml:"token"`
+	// (Optional) Prefix applied to the state path inside the bucket.
+	//
+	// This is only relevant when using a non-default workspace. Defaults to env:
 	// Experimental.
 	WorkspaceKeyPrefix *string `json:"workspaceKeyPrefix" yaml:"workspaceKeyPrefix"`
 }
